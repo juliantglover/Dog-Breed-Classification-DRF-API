b76d4d2b0008995a64a6ac1107f0ca7fd30c2822
Your Name <you@example.com>
2019-10-14 23:49:33 -0400

    Updated startServer script

--- startServer.sh
+++ startServer.sh
@@ -3,4 +3,4 @@ cd /Dog-Breed-Classification-DRF-API
 echo "yes" | python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
-exec gunicorn --workers 3 --bind unix:/apisocket/apisocket.sock dogbreedclassifier.wsgi:application
+exec gunicorn --workers 3 --bind unix:/apisocket/apisocket.sock deeplearningapp.wsgi:application
